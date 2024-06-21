42bb2a63df6f81bf58f22724562aec471307d451
veryrarestat <stmantaz@gmail.com>
2024-06-21 21:02:30 +0100

    Fixing Script

--- script/main.js
+++ script/main.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const audio = new Audio('./assets/loop.mp3');
     audio.volume = 0.3;
     audio.loop = true;
-    audio.play();
+    setTimeout(() => {
+        audio.play();
+    }, 500);
 });
